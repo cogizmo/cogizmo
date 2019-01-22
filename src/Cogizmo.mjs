c71e6042ba6a9d10309e52ec273af71068d47a05
Donald Atkinson <fuzzicallogic@outlook.com>
2019-01-22 01:18:11 -0700

    Create Cogizmo.mjs
    Create a separate ES6 module for #4. To ensure compatibility with non-module components, also added the ability to add the class to the global namespace.

---
+++ src/Cogizmo.mjs
@@ -0,0 +1,144 @@
+// Determine if the consumer wants to attach Cogizmo to the window/global. This
+// only works from a `<script type="module">` tag with a src attribute.
+let pollute = (() => {
+    let scr = null;
+    let url = import.meta.url;
+    document.querySelectorAll('script[type="module"]').forEach(el => {
+        let anchor = document.createElement('a');
+        anchor.setAttribute('href', el.src);
+        if (anchor.href === url)
+            scr = el;
+    });
+    return scr.hasAttribute('use-global');
+}) ();
+
+const _PROPERTIES_ = new WeakMap();
+export default class Cogizmo extends HTMLElement {
+    constructor() {
+        super();
+
+    // Attach HTML Template as soon as possible
+        const root = this.attachShadow({ mode: 'open' });
+        const instance = this.constructor.template.content.cloneNode(true);
+        root.appendChild(instance);
+    }
+
+    connectedCallback() {
+    // Feature Check
+        if ('function' === typeof super.connectedCallback)
+            super.connectedCallback();
+    }
+
+    static get observedAttributes() {
+    // Future proof inheritance
+        return [];
+    }
+
+    attributeChangedCallback(name, old, value) {
+    // Feature Check and follow convention
+        if ('function' === typeof super.attributeChangedCallback)
+            super.attributeChangedCallback(name, old, value);
+
+        /** @todo Convert attribute names to Camel Case */
+        if ('function' === typeof this[`on${name.charAt(0).toUpperCase()}${name.slice(1)}Changed`])
+            this[`on${name.charAt(0).toUpperCase()}${name.slice(1)}Changed`](old, value);
+    }
+
+    disconnectedCallback() {
+    // Feature Check and follow convention
+        if ('function' === typeof super.disconnectedCallback)
+            super.disconnectedCallback();
+    }
+
+    /**
+     * Tells Cogizmo SuperClass to collect resources and register the
+     * Custom Element with the browser.
+     */
+    static async manage() {
+        if (!!!_PROPERTIES_.get(this))
+            _PROPERTIES_.set(this, Object.create(null))
+
+    // document.currentScript still works when called from a non-module script.
+        let script = _PROPERTIES_.get(this).script = document.currentScript;
+
+        await this.template;
+
+        let tag = this.is;
+        if (script.hasAttribute('tagname'))
+            tag = script.getAttribute('tagname') || tag;
+
+        customElements.define(tag, this);
+
+    }
+
+    /**
+     * Retrieves a safe reference to the <script> element that loaded
+     * the Custom Element.
+     */
+    static get script() {
+        return _PROPERTIES_.get(this).script;
+    }
+
+    /**
+     * Root URL of the Custom Element. May be used to load external assets
+     * and dependencies.
+     */
+    static get path() {
+        let url = _PROPERTIES_.get(this).path;
+        if (!!!url) {
+        // Reverse-Engineer the path from the <script> element.
+            let a = document.createElement('a'),
+                href = _PROPERTIES_.get(this).script.src + '/../';
+            a.href = href;
+        // Store path locally to avoid external tampering
+            url = _PROPERTIES_.get(this).path = a.href;
+
+            /** @todo Insecure. Used for electron compatibility.
+             *  Add support for custom protocols.
+             */
+            // Support Electron file: protocol
+            if ('file:' === a.protocol
+            &&  'function' === typeof require
+            &&  'object' === typeof require('electron')) {
+                let {webFrame} = require('electron');
+                webFrame.registerURLSchemeAsPrivileged('file');
+            }
+        }
+        return url;
+    }
+
+    /**
+     * Returns the imported <template> used by the Custom Element.
+     */
+    static get template() {
+        if (!!!_PROPERTIES_.get(this).template)
+            _PROPERTIES_.get(this).template = getTemplate.call(this);
+        return _PROPERTIES_.get(this).template
+    }
+}
+!!pollute && (global.Cogizmo = Cogizmo);
+
+
+_PROPERTIES_.set(Cogizmo, Object.create(null));
+_PROPERTIES_.get(Cogizmo).scripts = new WeakMap();
+
+async function getTemplate() {
+    let response = await fetch(`${this.path}template.html`);
+    let txt = await response.text();
+
+    let html =  new DOMParser().parseFromString(txt, 'text/html');
+    let scripts = html.querySelectorAll('head > script');
+    Array.prototype.forEach.call(scripts, (script) => {
+        script.src = `${this.path}${script.getAttribute('src')}`
+        let imported = document.importNode(script, true);
+        document.head.appendChild(imported);
+    });
+
+    let tpl = _PROPERTIES_.get(this).template = html.querySelector('head > template');
+
+    // Support external stylesheets in templates
+    let link = tpl.content.querySelector('link');
+    if (link)
+        link.href = `${this.path}${link.getAttribute('href')}`;
+    return tpl;
+}
